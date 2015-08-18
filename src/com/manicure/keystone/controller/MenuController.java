60b8bdad73abc2d02b35b524bd2a11bb2f6b831c
Barrie <barrieshieh@gmail.com>
2015-08-18 21:34:53 +0800

    add comment pics

--- src/com/manicure/keystone/controller/MenuController.java
+++ src/com/manicure/keystone/controller/MenuController.java
@@ -51,16 +51,16 @@ public class MenuController extends BaseController {
 		String urlHome = URL_SNS_OAUTH2_REDIRECT.replace("REDIRECT_URI", UrlUtil.toUTF8(baseUrl + "/mobile/index")).replace("APPID", APP_ID).replace("SCOPE", "snsapi_base")
 				.replace("STATE", "STATE");
 		menuStr = menuStr.replace(MenuService.V2002_WEB_HOME, urlHome);
-		String urlOrder = URL_SNS_OAUTH2_REDIRECT.replace("REDIRECT_URI", UrlUtil.toUTF8(baseUrl + "/mobile/order")).replace("APPID", APP_ID).replace("SCOPE", "snsapi_base")
+		String urlOrder = URL_SNS_OAUTH2_REDIRECT.replace("REDIRECT_URI", UrlUtil.toUTF8(baseUrl + "/mobile/order-list")).replace("APPID", APP_ID).replace("SCOPE", "snsapi_base")
 				.replace("STATE", "STATE");
 		menuStr = menuStr.replace(MenuService.V3002_ORDER, urlOrder);
-		String urlNewArrived = URL_SNS_OAUTH2_REDIRECT.replace("REDIRECT_URI", UrlUtil.toUTF8(baseUrl + "/mobile/list")).replace("APPID", APP_ID).replace("SCOPE", "snsapi_base")
+		String urlNewArrived = URL_SNS_OAUTH2_REDIRECT.replace("REDIRECT_URI", UrlUtil.toUTF8(baseUrl + "/mobile/product-list")).replace("APPID", APP_ID).replace("SCOPE", "snsapi_base")
 				.replace("STATE", "STATE");
 		menuStr = menuStr.replace(MenuService.V1003_NEW_ARRIVED, urlNewArrived);
-		String urlLottery = URL_SNS_OAUTH2_REDIRECT.replace("REDIRECT_URI", UrlUtil.toUTF8(baseUrl + "/mobile/list?groupId=208216165&orderBy=sales&sort=desc")).replace("APPID", APP_ID).replace("SCOPE", "snsapi_base")
+		String urlLottery = URL_SNS_OAUTH2_REDIRECT.replace("REDIRECT_URI", UrlUtil.toUTF8(baseUrl + "/mobile/product-list?groupId=208216165&orderBy=sales&sort=desc")).replace("APPID", APP_ID).replace("SCOPE", "snsapi_base")
 				.replace("STATE", "STATE");
 		menuStr = menuStr.replace(MenuService.V1001_LOTTERY, urlLottery);
-		String urlAddress = URL_SNS_OAUTH2_REDIRECT.replace("REDIRECT_URI", UrlUtil.toUTF8(baseUrl + "/mobile/home")).replace("APPID", APP_ID).replace("SCOPE", "snsapi_base")
+		String urlAddress = URL_SNS_OAUTH2_REDIRECT.replace("REDIRECT_URI", UrlUtil.toUTF8(baseUrl + "/mobile/shop-list")).replace("APPID", APP_ID).replace("SCOPE", "snsapi_base")
 				.replace("STATE", "STATE");
 		menuStr = menuStr.replace(MenuService.V3001_ADDREDD, urlAddress);
 		logger.info(menuStr);
